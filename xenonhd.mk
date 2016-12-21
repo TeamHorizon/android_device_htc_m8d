bf78a6f07aae06c029def6a97854d2a98044bb98
ljjehl <ljjehl@gmail.com>
2016-12-21 22:02:52 +0100

    Give m8d some Xenon loving
    Change-Id: I74a6bc6534f44a1839423285b7575ea8c6b3f704

---
+++ xenonhd.mk
@@ -0,0 +1,16 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
+
+$(call inherit-product, device/htc/m8/full_m8.mk)
+
+
+# Set those variables here to overwrite the inherited values.
+PRODUCT_BRAND := htc
+PRODUCT_DEVICE := m8d
+PRODUCT_MANUFACTURER := HTC
+PRODUCT_MODEL := One
+PRODUCT_NAME := xenonhd_m8d
+
+# Inherit from m8-common
+$(call inherit-product, device/htc/m8-common/m8-common.mk)
+
