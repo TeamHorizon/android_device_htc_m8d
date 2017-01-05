b1f54103bd0d9109e25f7bc133eea5a5cdf4d3c0
ljjehl <ljjehl@gmail.com>
2017-01-06 00:16:38 +0100

    Pull m8d vendor
    Change-Id: I2371493307fc1b86e125da08a1b4e98d04a2cb55

--- xenonhd.mk
+++ xenonhd.mk
@@ -3,6 +3,8 @@ $(call inherit-product, vendor/xenonhd/config/common_full_phone.mk)
 
 $(call inherit-product, device/htc/m8/full_m8.mk)
 
+# Inherit from m8d device
+$(call inherit-product, device/htc/m8d/device.mk)
 
 # Set those variables here to overwrite the inherited values.
 PRODUCT_BRAND := htc
@@ -10,7 +12,4 @@ PRODUCT_DEVICE := m8d
 PRODUCT_MANUFACTURER := HTC
 PRODUCT_MODEL := One
 PRODUCT_NAME := xenonhd_m8d
-
-# Inherit from m8-common
-$(call inherit-product, device/htc/m8-common/m8-common.mk)
 
